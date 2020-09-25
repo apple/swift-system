f10529690351f370e564df0ba31a543a9530e353
Michael Ilseman <milseman@apple.com>
2020-09-25 09:00:53 -0700

    Syscall mock testing
    Adds mocking and tracing infrastructure, test
    infrastructure, and trace-based tests.

--- Sources/SystemInternals/Exports.swift
+++ Sources/SystemInternals/Exports.swift
@@ -35,50 +35,6 @@ public var system_errno: CInt {
 }
 #endif
 
-public func system_open(_ path: UnsafePointer<CChar>, _ oflag: Int32) -> CInt {
-  open(path, oflag)
-}
-
-public func system_open(
-  _ path: UnsafePointer<CChar>, _ oflag: Int32, _ mode: mode_t
-) -> CInt {
-  open(path, oflag, mode)
-}
-
-public func system_close(_ fd: Int32) -> Int32 {
-  close(fd)
-}
-
-public func system_read(
-  _ fd: Int32, _ buf: UnsafeMutableRawPointer!, _ nbyte: Int
-) -> Int {
-  read(fd, buf, nbyte)
-}
-
-public func system_pread(
-  _ fd: Int32, _ buf: UnsafeMutableRawPointer!, _ nbyte: Int, _ offset: off_t
-) -> Int {
-  pread(fd, buf, nbyte, offset)
-}
-
-public func system_lseek(
-  _ fd: Int32, _ off: off_t, _ whence: Int32
-) -> off_t {
-  lseek(fd, off, whence)
-}
-
-public func system_write(
-  _ fd: Int32, _ buf: UnsafeRawPointer!, _ nbyte: Int
-) -> Int {
-  write(fd, buf, nbyte)
-}
-
-public func system_pwrite(
-  _ fd: Int32, _ buf: UnsafeRawPointer!, _ nbyte: Int, _ offset: off_t
-) -> Int {
-  pwrite(fd, buf, nbyte, offset)
-}
-
 // MARK: C stdlib decls
 
 public func system_strerror(_ __errnum: Int32) -> UnsafeMutablePointer<Int8>! {
