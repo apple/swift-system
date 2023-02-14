16a277564503ba390ae57ceb8728cf96c114fbfb
Daniel Loffgren <loffgren@apple.com>
2023-02-13 17:53:42 -0800

    Annotate entire Mach namespace as being available in System 1.3.0

--- Sources/System/MachPort.swift
+++ Sources/System/MachPort.swift
@@ -23,6 +23,7 @@ private func _machPrecondition(
   precondition(kr == expected, file: file, line: line)
 }
 
+/*System 1.3.0, @available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)*/
 @frozen
 public enum Mach {
   @_moveOnly
