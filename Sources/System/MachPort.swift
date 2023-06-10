af4544c86920634166223e4e3f6fe99db9e2dcaf
Guillaume Lessard <guillaume.lessard@apple.com>
2023-06-09 17:56:36 -0700

    Re-enable Mach.Port

--- Sources/System/MachPort.swift
+++ Sources/System/MachPort.swift
@@ -7,7 +7,7 @@
  See https://swift.org/LICENSE.txt for license information
 */
 
-#if false && swift(>=5.8) && $MoveOnly && (os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
+#if swift(>=5.9) && (os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
 
 import Darwin.Mach
 
