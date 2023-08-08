1f821a8539af62aab858f356ab9aa21592742ccb
Lucy Satheesan <lsatheesan@apple.com>
2023-08-08 09:44:32 -0700

    AsyncSequence draft implementation

--- Sources/System/AsyncFileDescriptor.swift
+++ Sources/System/AsyncFileDescriptor.swift
@@ -69,3 +69,55 @@ public class AsyncFileDescriptor {
         }
     }
 }
+
+extension AsyncFileDescriptor: AsyncSequence {
+    public func makeAsyncIterator() -> FileIterator {
+        return .init(self)
+    }
+
+    public typealias AsyncIterator = FileIterator
+    public typealias Element = UInt8
+}
+
+public struct FileIterator: AsyncIteratorProtocol {
+    @usableFromInline let file: AsyncFileDescriptor
+    @usableFromInline var buffer: IORingBuffer
+    @usableFromInline var done: Bool
+
+    @usableFromInline internal var currentByte: UnsafeRawPointer?
+    @usableFromInline internal var lastByte: UnsafeRawPointer?
+
+    init(_ file: AsyncFileDescriptor) {
+        self.file = file
+        self.buffer = file.ring.getBuffer()!
+        self.done = false
+    }
+
+    @inlinable @inline(__always)
+    public mutating func nextBuffer() async throws {
+        let buffer = self.buffer
+
+        let bytesRead = try await file.read(into: .registered(buffer))
+        if _fastPath(bytesRead != 0) {
+            let bufPointer = buffer.unsafeBuffer.baseAddress.unsafelyUnwrapped
+            self.currentByte = UnsafeRawPointer(bufPointer)
+            self.lastByte = UnsafeRawPointer(bufPointer.advanced(by: Int(bytesRead)))
+        } else {
+            self.done = true
+        }
+    }
+
+    @inlinable @inline(__always) @_unsafeInheritExecutor
+    public mutating func next() async throws -> UInt8? {
+        if _fastPath(currentByte != lastByte) {
+            // SAFETY: both pointers should be non-nil if they're not equal
+            let byte = currentByte.unsafelyUnwrapped.load(as: UInt8.self)
+            currentByte = currentByte.unsafelyUnwrapped + 1
+            return byte
+        } else if done {
+            return nil
+        }
+        try await nextBuffer()
+        return try await next()
+    }
+}
