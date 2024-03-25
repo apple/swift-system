b45ea644251279bf8cf4269143c538fb503c8bae
Alastair Houghton <ahoughton@apple.com>
2024-03-25 18:28:18 +0000

    Refactor FilePathTemp, add tests for permission code.
    Broke `FilePathTemp.swift` into separate files for POSIX and
    Windows.
    
    Added tests for permissions code on Windows.
    
    Added leading underscores to some function names.
    
    Made some other functions `fileprivate`.
    
    rdar://125087707

--- Sources/System/FilePath/FilePathTemp.swift
+++ Sources/System/FilePath/FilePathTemp.swift
@@ -9,13 +9,22 @@
 
 // MARK: - API
 
-public func withTemporaryPath<R>(
+/// Create a temporary path for the duration of the closure.
+///
+/// - Parameters:
+///   - basename: The base name for the temporary path.
+///   - body: The closure to execute.
+///
+/// Creates a temporary directory with a name based on the given `basename`,
+/// executes `body`, passing in the path of the created directory, then
+/// deletes the directory and all of its contents before returning.
+public func withTemporaryFilePath<R>(
   basename: FilePath.Component,
   _ body: (FilePath) throws -> R
 ) throws -> R {
   let temporaryDir = try createUniqueTemporaryDirectory(basename: basename)
   defer {
-    try? recursiveRemove(at: temporaryDir)
+    try? _recursiveRemove(at: temporaryDir)
   }
 
   return try body(temporaryDir)
@@ -23,230 +32,20 @@ public func withTemporaryPath<R>(
 
 // MARK: - Internals
 
-#if os(Windows)
-import WinSDK
-
-fileprivate func getTemporaryDirectory() throws -> FilePath {
-  return try withUnsafeTemporaryAllocation(of: CInterop.PlatformChar.self,
-                                           capacity: Int(MAX_PATH) + 1) {
-    buffer in
-
-    guard GetTempPath2W(DWORD(buffer.count), buffer.baseAddress) != 0 else {
-      throw Errno(windowsError: GetLastError())
-    }
-
-    return FilePath(SystemString(platformString: buffer.baseAddress!))
-  }
-}
-
-fileprivate func forEachFile(
-  at path: FilePath,
-  _ body: (WIN32_FIND_DATAW) throws -> ()
-) rethrows {
-  let searchPath = path.appending("\\*")
-
-  try searchPath.withPlatformString { szPath in
-    var findData = WIN32_FIND_DATAW()
-    let hFind = FindFirstFileW(szPath, &findData)
-    if hFind == INVALID_HANDLE_VALUE {
-      throw Errno(windowsError: GetLastError())
-    }
-    defer {
-      FindClose(hFind)
-    }
-
-    repeat {
-      // Skip . and ..
-      if findData.cFileName.0 == 46
-           && (findData.cFileName.1 == 0
-                 || (findData.cFileName.1 == 46
-                       && findData.cFileName.2 == 0)) {
-        continue
-      }
-
-      try body(findData)
-    } while FindNextFileW(hFind, &findData)
-  }
-}
-
-fileprivate func recursiveRemove(at path: FilePath) throws {
-  // First, deal with subdirectories
-  try forEachFile(at: path) { findData in
-    if (findData.dwFileAttributes & DWORD(FILE_ATTRIBUTE_DIRECTORY)) != 0 {
-      let name = withUnsafeBytes(of: findData.cFileName) {
-        return SystemString(platformString: $0.assumingMemoryBound(
-                              to: CInterop.PlatformChar.self).baseAddress!)
-      }
-      let component = FilePath.Component(name)!
-      let subpath = path.appending(component)
-
-      try recursiveRemove(at: subpath)
-    }
-  }
-
-  // Now delete everything else
-  try forEachFile(at: path) { findData in
-    let name = withUnsafeBytes(of: findData.cFileName) {
-      return SystemString(platformString: $0.assumingMemoryBound(
-                            to: CInterop.PlatformChar.self).baseAddress!)
-    }
-    let component = FilePath.Component(name)!
-    let subpath = path.appending(component)
-
-    if (findData.dwFileAttributes & DWORD(FILE_ATTRIBUTE_DIRECTORY)) == 0 {
-      try subpath.withPlatformString {
-        if !DeleteFileW($0) {
-          throw Errno(windowsError: GetLastError())
-        }
-      }
-    }
-  }
-
-  // Finally, delete the parent
-  try path.withPlatformString {
-    if !RemoveDirectoryW($0) {
-      throw Errno(windowsError: GetLastError())
-    }
-  }
-}
-
-#else
-fileprivate func getTemporaryDirectory() throws -> FilePath {
-  #if SYSTEM_PACKAGE_DARWIN
-  var capacity = 1024
-  while true {
-    let path: FilePath? = withUnsafeTemporaryAllocation(
-      of: CInterop.PlatformChar.self,
-      capacity: capacity
-    ) { buffer in
-      let len = system_confstr(SYSTEM_CS_DARWIN_USER_TEMP_DIR,
-                               buffer.baseAddress!,
-                               buffer.count)
-      if len == 0 {
-        // Fall back to "/tmp" if we can't read the temp directory
-        return "/tmp"
-      }
-      // If it was truncated, increase capaciy and try again
-      if len > buffer.count {
-        capacity = len
-        return nil
-      }
-      return FilePath(SystemString(platformString: buffer.baseAddress!))
-    }
-    if let path = path {
-      return path
-    }
-  }
-  #else
-  return "/tmp"
-  #endif
-}
-
-fileprivate func recursiveRemove(at path: FilePath) throws {
-  let dirfd = try FileDescriptor.open(path, .readOnly, options: .directory)
-  defer {
-    try? dirfd.close()
-  }
-
-  let dot: (CInterop.PlatformChar, CInterop.PlatformChar) = (46, 0)
-  try withUnsafeBytes(of: dot) {
-    try recursiveRemove(
-      in: dirfd.rawValue,
-      path: $0.assumingMemoryBound(to: CInterop.PlatformChar.self).baseAddress!
-    )
-  }
-
-  try path.withPlatformString {
-    if system_rmdir($0) != 0 {
-      throw Errno.current
-    }
-  }
-}
-
-fileprivate func impl_opendirat(
-  _ dirfd: CInt,
-  _ path: UnsafePointer<CInterop.PlatformChar>
-) -> UnsafeMutablePointer<system_DIR>? {
-  let fd = system_openat(dirfd, path,
-                         FileDescriptor.AccessMode.readOnly.rawValue
-                           | FileDescriptor.OpenOptions.directory.rawValue)
-  if fd < 0 {
-    return nil
-  }
-  return system_fdopendir(fd)
-}
-
-fileprivate func forEachFile(
-  in dirfd: CInt, path: UnsafePointer<CInterop.PlatformChar>,
-  _ body: (system_dirent) throws -> ()
-) throws {
-  guard let dir = impl_opendirat(dirfd, path) else {
-    throw Errno.current
-  }
-  defer {
-    _ = system_closedir(dir)
-  }
-
-  while let dirent = system_readdir(dir) {
-    // Skip . and ..
-    if dirent.pointee.d_name.0 == 46
-         && (dirent.pointee.d_name.1 == 0
-               || (dirent.pointee.d_name.1 == 46
-                     && dirent.pointee.d_name.2 == 0)) {
-      continue
-    }
-
-    try body(dirent.pointee)
-  }
-}
-
-internal func recursiveRemove(
-  in dirfd: CInt,
-  path: UnsafePointer<CInterop.PlatformChar>
-) throws {
-  // First, deal with subdirectories
-  try forEachFile(in: dirfd, path: path) { dirent in
-    if dirent.d_type == SYSTEM_DT_DIR {
-      try withUnsafeBytes(of: dirent.d_name) {
-        try recursiveRemove(
-          in: dirfd,
-          path: $0.assumingMemoryBound(to: CInterop.PlatformChar.self)
-            .baseAddress!
-        )
-      }
-    }
-  }
-
-  // Now delete the contents of this directory
-  try forEachFile(in: dirfd, path: path) { dirent in
-    let flag: CInt
-
-    if dirent.d_type == SYSTEM_DT_DIR {
-      flag = SYSTEM_AT_REMOVE_DIR
-    } else {
-      flag = 0
-    }
-
-    let result = withUnsafeBytes(of: dirent.d_name) {
-      system_unlinkat(dirfd,
-                      $0.assumingMemoryBound(to: CInterop.PlatformChar.self)
-                        .baseAddress!,
-                      flag)
-    }
-
-    if result != 0 {
-      throw Errno.current
-    }
-  }
-}
-#endif
-
 fileprivate let base64 = Array<UInt8>(
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".utf8
 )
 
-fileprivate func makeTempDirectory(at: FilePath) throws -> Bool {
-  return try at.withPlatformString {
+/// Create a directory that is only accessible to the current user.
+///
+/// - Parameters:
+///   - path: The path of the directory to create.
+/// - Returns: `true` if a new directory was created.
+///
+/// This function will throw if there is an error, except if the error
+/// is that the directory exists, in which case it returns `false`.
+fileprivate func makeLockedDownDirectory(at path: FilePath) throws -> Bool {
+  return try path.withPlatformString {
     if system_mkdir($0, 0o700) == 0 {
       return true
     }
@@ -259,6 +58,11 @@ fileprivate func makeTempDirectory(at: FilePath) throws -> Bool {
   }
 }
 
+/// Generate a random string of base64 filename safe characters.
+///
+/// - Parameters:
+///   - length: The number of characters in the returned string.
+/// - Returns: A random string of length `length`.
 fileprivate func createRandomString(length: Int) -> String {
   return String(
     decoding: (0..<length).map{
@@ -268,16 +72,25 @@ fileprivate func createRandomString(length: Int) -> String {
   )
 }
 
-internal func createUniqueTemporaryDirectory(
+/// Given a base name, create a uniquely named temporary directory.
+///
+/// - Parameters:
+///   - basename: The base name for the new directory.
+/// - Returns: The path to the new directory.
+///
+/// Creates a directory in the system temporary directory whose name
+/// starts with `basename`, followed by a `.` and then a random
+/// string of characters.
+fileprivate func createUniqueTemporaryDirectory(
   basename: FilePath.Component
 ) throws -> FilePath {
-  var tempDir = try getTemporaryDirectory()
+  var tempDir = try _getTemporaryDirectory()
   tempDir.append(basename)
 
   while true {
     tempDir.extension = createRandomString(length: 16)
 
-    if try makeTempDirectory(at: tempDir) {
+    if try makeLockedDownDirectory(at: tempDir) {
       return tempDir
     }
   }
---
+++ Tests/SystemTests/FileOperationsTestWindows.swift
@@ -0,0 +1,241 @@
+/*
+ This source file is part of the Swift System open source project
+
+ Copyright (c) 2024 Apple Inc. and the Swift System project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See https://swift.org/LICENSE.txt for license information
+*/
+
+import XCTest
+
+#if os(Windows)
+
+#if SYSTEM_PACKAGE
+import SystemPackage
+#else
+import System
+#endif
+
+import WinSDK
+
+@available(iOS 8, *)
+final class FileOperationsTestWindows: XCTestCase {
+  private let r = ACCESS_MASK(
+    FILE_READ_ATTRIBUTES
+      | FILE_READ_DATA
+      | FILE_READ_EA
+      | STANDARD_RIGHTS_READ
+      | SYNCHRONIZE
+  )
+  private let w = ACCESS_MASK(
+    FILE_APPEND_DATA
+      | FILE_WRITE_ATTRIBUTES
+      | FILE_WRITE_DATA
+      | FILE_WRITE_EA
+      | STANDARD_RIGHTS_WRITE
+      | SYNCHRONIZE
+  )
+  private let x = ACCESS_MASK(
+    FILE_EXECUTE
+      | FILE_READ_ATTRIBUTES
+      | STANDARD_RIGHTS_EXECUTE
+      | SYNCHRONIZE
+  )
+
+  private struct Test {
+    var permissions: CModeT
+    var ownerAccess: ACCESS_MASK
+    var groupAccess: ACCESS_MASK
+    var otherAccess: ACCESS_MASK
+
+    init(_ permissions: CModeT,
+         _ ownerAccess: ACCESS_MASK,
+         _ groupAccess: ACCESS_MASK,
+         _ otherAccess: ACCESS_MASK) {
+      self.permissions = permissions
+      self.ownerAccess = ownerAccess
+      self.groupAccess = groupAccess
+      self.otherAccess = otherAccess
+    }
+  }
+
+  /// Retrieve the owner, group and other access masks for a given file.
+  ///
+  /// - Parameters:
+  ///   - path: The path to the file to inspect
+  /// - Returns: A tuple of ACCESS_MASK values.
+  func getAccessMasks(
+    path: FilePath
+  ) -> (ACCESS_MASK, ACCESS_MASK, ACCESS_MASK) {
+    var SIDAuthWorld = SID_IDENTIFIER_AUTHORITY(Value: (0, 0, 0, 0, 0, 1))
+    var psidEveryone: PSID? = nil
+
+    XCTAssert(AllocateAndInitializeSid(&SIDAuthWorld, 1,
+                                       DWORD(SECURITY_WORLD_RID),
+                                       0, 0, 0, 0, 0, 0, 0,
+                                       &psidEveryone))
+    defer {
+      FreeSid(psidEveryone)
+    }
+
+    var everyone = TRUSTEE_W(
+      pMultipleTrustee: nil,
+      MultipleTrusteeOperation: NO_MULTIPLE_TRUSTEE,
+      TrusteeForm: TRUSTEE_IS_SID,
+      TrusteeType: TRUSTEE_IS_GROUP,
+      ptstrName:
+        psidEveryone!.assumingMemoryBound(to: CInterop.PlatformChar.self)
+    )
+
+    return path.withPlatformString { objectName in
+      var psidOwner: PSID? = nil
+      var psidGroup: PSID? = nil
+      var pDacl: PACL? = nil
+      var pSD: PSECURITY_DESCRIPTOR? = nil
+
+      XCTAssertEqual(GetNamedSecurityInfoW(
+                       objectName,
+                       SE_FILE_OBJECT,
+                       SECURITY_INFORMATION(
+                         DACL_SECURITY_INFORMATION
+                           | GROUP_SECURITY_INFORMATION
+                           | OWNER_SECURITY_INFORMATION
+                       ),
+                       &psidOwner,
+                       &psidGroup,
+                       &pDacl,
+                       nil,
+                       &pSD), DWORD(ERROR_SUCCESS))
+      defer {
+        LocalFree(pSD)
+      }
+
+      var owner = TRUSTEE_W(
+        pMultipleTrustee: nil,
+        MultipleTrusteeOperation: NO_MULTIPLE_TRUSTEE,
+        TrusteeForm: TRUSTEE_IS_SID,
+        TrusteeType: TRUSTEE_IS_USER,
+        ptstrName:
+          psidOwner!.assumingMemoryBound(to: CInterop.PlatformChar.self)
+      )
+      var group = TRUSTEE_W(
+        pMultipleTrustee: nil,
+        MultipleTrusteeOperation: NO_MULTIPLE_TRUSTEE,
+        TrusteeForm: TRUSTEE_IS_SID,
+        TrusteeType: TRUSTEE_IS_GROUP,
+        ptstrName:
+          psidGroup!.assumingMemoryBound(to: CInterop.PlatformChar.self)
+      )
+
+      var ownerAccess = ACCESS_MASK(0)
+      var groupAccess = ACCESS_MASK(0)
+      var otherAccess = ACCESS_MASK(0)
+
+      XCTAssertEqual(GetEffectiveRightsFromAclW(
+                       pDacl,
+                       &owner,
+                       &ownerAccess), DWORD(ERROR_SUCCESS))
+      XCTAssertEqual(GetEffectiveRightsFromAclW(
+                       pDacl,
+                       &group,
+                       &groupAccess), DWORD(ERROR_SUCCESS))
+      XCTAssertEqual(GetEffectiveRightsFromAclW(
+                       pDacl,
+                       &everyone,
+                       &otherAccess), DWORD(ERROR_SUCCESS))
+
+      return (ownerAccess, groupAccess, otherAccess)
+    }
+  }
+
+  private func runTests(_ tests: [Test], at path: FilePath) throws {
+    for test in tests {
+      let octal = String(test.permissions, radix: 8)
+      let testPath = path.appending("test-\(octal).txt")
+      let fd = try FileDescriptor.open(
+        testPath,
+        .readWrite,
+        options: [.create, .truncate],
+        permissions: FilePermissions(rawValue: test.permissions)
+      )
+      _ = try fd.closeAfter {
+        try fd.writeAll("Hello World".utf8)
+      }
+
+      let (ownerAccess, groupAccess, otherAccess)
+        = getAccessMasks(path: testPath)
+
+      XCTAssertEqual(ownerAccess, test.ownerAccess)
+      XCTAssertEqual(groupAccess, test.groupAccess)
+      XCTAssertEqual(otherAccess, test.otherAccess)
+    }
+  }
+
+  /// Test that the umask works properly
+  func testUmask() throws {
+    // Default mask should be 0o022
+    XCTAssertEqual(FilePermissions.creationMask, [.groupWrite, .otherWrite])
+
+    try withTemporaryFilePath(basename: "testUmask") { path in
+      let tests = [
+        Test(0o000, 0, 0, 0),
+        Test(0o700, r|w|x, 0, 0),
+        Test(0o770, r|w|x, r|x, 0),
+        Test(0o777, r|w|x, r|x, r|x)
+      ]
+
+      try runTests(tests, at: path)
+    }
+
+    try FilePermissions.withCreationMask([.groupWrite, .groupExecute,
+                                          .otherWrite, .otherExecute]) {
+      try withTemporaryFilePath(basename: "testUmask") { path in
+        let tests = [
+          Test(0o000, 0, 0, 0),
+          Test(0o700, r|w|x, 0, 0),
+          Test(0o770, r|w|x, r, 0),
+          Test(0o777, r|w|x, r, r)
+        ]
+
+        try runTests(tests, at: path)
+      }
+    }
+  }
+
+  /// Test that setting permissions on a file works as expected
+  func testPermissions() throws {
+    try FilePermissions.withCreationMask([]) {
+      try withTemporaryFilePath(basename: "testPermissions") { path in
+        let tests = [
+          Test(0o000, 0, 0, 0),
+
+          Test(0o400, r, 0, 0),
+          Test(0o200, w, 0, 0),
+          Test(0o100, x, 0, 0),
+          Test(0o040, 0, r, 0),
+          Test(0o020, 0, w, 0),
+          Test(0o010, 0, x, 0),
+          Test(0o004, 0, 0, r),
+          Test(0o002, 0, 0, w),
+          Test(0o001, 0, 0, x),
+
+          Test(0o700, r|w|x, 0, 0),
+          Test(0o770, r|w|x, r|w|x, 0),
+          Test(0o777, r|w|x, r|w|x, r|w|x),
+
+          Test(0o755, r|w|x, r|x, r|x),
+          Test(0o644, r|w, r, r),
+
+          Test(0o007, 0, 0, r|w|x),
+          Test(0o070, 0, r|w|x, 0),
+          Test(0o077, 0, r|w|x, r|w|x),
+        ]
+
+        try runTests(tests, at: path)
+      }
+    }
+  }
+}
+
+#endif // os(Windows)
