13822870366d7d35f4987c6bc54817c90f22b347
Guillaume Lessard <guillaume.lessard@apple.com>
2026-01-06 16:49:36 -0800

    Use swift-testing to test compatibility adaptors

--- Tests/SystemCompatibilityAdaptorTests/AdaptorTests.swift
+++ Tests/SystemCompatibilityAdaptorTests/AdaptorTests.swift
@@ -1,30 +1,34 @@
 #if canImport(System) && canImport(SystemPackage)
 
-import XCTest
+import Testing
 
 import SystemPackage
 import System
 
 import SystemCompatibilityAdaptors
 
-final class SystemCompatibilityAdaptorTests: XCTestCase {
+@Suite("CompatibilityAdaptors")
+private struct CompatibilityAdaptorTests {
 
-  func testFilePathAdaptor() {
+  @available(System 0.0.2, *)
+  @Test
+  func filePathAdaptor() throws {
     let fp = SystemPackage.FilePath("/bar")
 
     let sfp = System.FilePath(converting: fp)
 
     // If we had access to the underlying array as API, this would look less silly
-    XCTAssertEqual(fp, SystemPackage.FilePath(converting: sfp))
+    #expect(fp == SystemPackage.FilePath(converting: sfp))
   }
 
-  func testFileDescriptorAdaptor() throws {
+  @available(System 0.0.1, *)
+  @Test
+  func fileDescriptorAdaptor() throws {
     let fd = try SystemPackage.FileDescriptor.standardInput.duplicate()
 
     let sfd: System.FileDescriptor = System.FileDescriptor(converting: fd)
 
-    print(fd.rawValue)
-    XCTAssertEqual(fd.rawValue, sfd.rawValue)
+    #expect(fd.rawValue == sfd.rawValue)
   }
 }
 #endif // canImport(System) && canImport(SystemPackage)
