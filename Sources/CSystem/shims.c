f5bc74eeb8781941863246808f29f3c59258dffd
Guillaume Lessard <guillaume.lessard@apple.com>
2026-07-10 15:41:51 -0700

    Implement dup3/pipe2 wrappers and types for Darwin

--- Sources/CSystem/shims.c
+++ Sources/CSystem/shims.c
@@ -31,24 +31,40 @@
 #define HAVE_PIPE2_DUP3
 #endif
 
+#if defined(__APPLE__)
+#include <Availability.h>
+#if defined(__MAC_27_0)
+#include <unistd.h>
+#define HAVE_PIPE2_DUP3
+#endif
+#endif // defined(__APPLE__)
+
 // Wrappers are required because _GNU_SOURCE causes a conflict with other imports when defined in CSystemLinux.h
 
 #if !defined(_WIN32)
 extern int csystem_posix_pipe2(int fildes[2], int flag) {
-    #ifdef HAVE_PIPE2_DUP3
+  #if defined(__APPLE__) && defined(HAVE_PIPE2_DUP3)
+  if (__builtin_available(macOS 27, iOS 27, tvOS 27, watchOS 27, visionOS 27, *))
     return pipe2(fildes, flag);
-    #else
-    errno = ENOSYS;
-    return -1;
-    #endif
+  __builtin_trap();
+  #elif defined(HAVE_PIPE2_DUP3)
+  return pipe2(fildes, flag);
+  #else
+  errno = ENOSYS;
+  return -1;
+  #endif
 }
 #endif // !defined(_WIN32)
 
 extern int csystem_posix_dup3(int fildes, int fildes2, int flag) {
-    #ifdef HAVE_PIPE2_DUP3
+  #if defined(__APPLE__) && defined(HAVE_PIPE2_DUP3)
+  if (__builtin_available(macOS 27, iOS 27, tvOS 27, watchOS 27, visionOS 27, *))
     return dup3(fildes, fildes2, flag);
-    #else
-    errno = ENOSYS;
-    return -1;
-    #endif
+  __builtin_trap();
+  #elif defined(HAVE_PIPE2_DUP3)
+  return dup3(fildes, fildes2, flag);
+  #else
+  errno = ENOSYS;
+  return -1;
+  #endif
 }
