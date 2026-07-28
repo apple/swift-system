4df226b09887be1629aff65eec63f86363ee9942
Guillaume Lessard <guillaume.lessard@apple.com>
2026-07-28 14:49:03 -0700

    Fix copyright notices

--- Sources/System/IORing/IOCompletion.swift
+++ Sources/System/IORing/IOCompletion.swift
@@ -1,3 +1,12 @@
+/*
+ This source file is part of the Swift System open source project
+
+ Copyright (c) 2023 - 2025 Apple Inc. and the Swift System project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See https://swift.org/LICENSE.txt for license information
+*/
+
 #if compiler(>=6.2) && $Lifetimes
 #if os(Linux)
 
